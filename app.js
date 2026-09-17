ad159dd0e06cadf10b334c75e4e2645819958414
cohng-hub <cohng-hub@github.com>
2026-09-17 16:55:04 +0900

    [Auto-Sync] Update official Korea Gold Exchange live rates (2026.09.17 16:55)

--- app.js
+++ app.js
@@ -5,29 +5,29 @@
 
 // Official Live Rates from Korea Gold Exchange (koreagoldx.co.kr 100% Exact Official Rates)
 let REALTIME_STANDARD_RATES = {
-  "24K_buy": 841000,
-  "24K_sell": 708000,
-  "18K_sell": 520400,
-  "14K_sell": 403600,
-  "PT_buy": 342000,
-  "PT_sell": 278000,
-  "AG_buy": 11890,
-  "AG_sell": 9900
+  "24K_buy": 849000,
+  "24K_sell": 710000,
+  "18K_sell": 521900,
+  "14K_sell": 404700,
+  "PT_buy": 349000,
+  "PT_sell": 283000,
+  "AG_buy": 12020,
+  "AG_sell": 10000
 };
 
 let REALTIME_RATE_CHANGES = {
-  "24K_buy_diff": 2000,
-  "24K_buy_per": 0.24,
-  "24K_sell_diff": 0,
-  "24K_sell_per": 0,
-  "18K_sell_diff": 0,
-  "18K_sell_per": 0,
-  "14K_sell_diff": 0,
-  "14K_sell_per": 0,
-  "PT_sell_diff": 0,
-  "PT_sell_per": 0,
-  "AG_sell_diff": -30,
-  "AG_sell_per": -0.3,
+  "24K_buy_diff": 10000,
+  "24K_buy_per": 1.18,
+  "24K_sell_diff": 2000,
+  "24K_sell_per": 0.28,
+  "18K_sell_diff": 1500,
+  "18K_sell_per": 0.29,
+  "14K_sell_diff": 1100,
+  "14K_sell_per": 0.27,
+  "PT_sell_diff": 5000,
+  "PT_sell_per": 1.77,
+  "AG_sell_diff": 70,
+  "AG_sell_per": 0.7,
   "date": "2026.09.17"
 };
 
